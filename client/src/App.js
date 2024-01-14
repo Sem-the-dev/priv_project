58102b095dbe4801ba3b79f673146b969e773b3f
Sem-the-dev <semhar.tesfu@outlook.com>
2024-01-14 11:52:43 +0000

    add carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function App() {
   const [itemData, setItemData] = useState([]);
@@ -11,17 +13,24 @@ function App() {
           return response.json();
         })
         .then((data) => {
-          console.log(data.data);
+          // console.log(data.data);
           setItemData(data.data);
 
               });
   }, []);
-    console.log(itemData)
+
     return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome</h1>
-
+          <Carousel>
+              {itemData.map((item) => (
+                  <div key={item.id}>
+                      <img src={item.image} />
+                      <p className="legend">{item.name} --> {item.discount_percentage}% discount</p>
+                  </div>
+              ))}
+          </Carousel>
       </header>
     </div>
   );
